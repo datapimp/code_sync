77d23b98302be18a689fec83ebc0333766a1cb9f
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-05-14 21:39:01 -0500

    updating build config

--- site/config.rb
+++ site/config.rb
@@ -12,23 +12,7 @@ set :markdown, :fenced_code_blocks => true, :smartypants => true
 activate :syntax, :cssclass => "codehilite"
 
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :cache_buster
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
 end
